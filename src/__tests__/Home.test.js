befdad46a8367d1e096524901406219de8c0a067
samwhale <samuel.estrella@formidable.com>
2020-06-22 21:44:36 -0600

    Add Tests

--- src/__tests__/Home.test.js
+++ src/__tests__/Home.test.js
@@ -1,6 +1,27 @@
+import React from 'react';
+
 import Home from '../components/Home';
+import { render, fireEvent } from '@testing-library/react';
+
+const USER = {
+  username: 'testing'
+}
 
 describe('<Home />', () => {
-  test.todo('if Home displays the username');
-  test.todo('if clicking the button signs the user out');
+  test('if Home displays the username', () => {
+    const { getByRole } = render(<Home user={USER} />);
+    
+    const heading = getByRole('heading');
+    expect(heading.textContent).toEqual(`Yay! You're signed in ${USER.username}!`);
+  });
+
+  test('if clicking the button signs the user out', () => {
+    const onSignOut = jest.fn();
+
+    const { getByText } = render(<Home onSignOut={onSignOut} user={USER} />);
+    const button = getByText('Sign Out');
+    fireEvent.click(button);
+
+    expect(onSignOut).toHaveBeenCalled();
+  });
 });
--- src/__tests__/SignInForm.test.js
+++ src/__tests__/SignInForm.test.js
@@ -1,7 +1,65 @@
+
+import React from 'react';
+import { act, render, fireEvent } from '@testing-library/react';
+import { signUserIn } from '../utils/sign-in';
+
 import SignInForm from '../components/SignInForm';
 
+jest.mock('../utils/sign-in', () => ({
+  signUserIn: jest.fn()
+}));
+
+
 describe('<SignInForm />', () => {
-  test.todo('if clicking the sign in button signs the user in');
-  test.todo('if SignIn shows an error when sign in fails');
-  test.todo('if SignIn shows a loading animation while waiting for sign in to complete');
+  let onSubmit = jest.fn();
+
+  beforeEach(() => {
+    signUserIn.mockImplementation(() => Promise.resolve({
+      statusCode: 200,
+      user: {
+        username: 'test'
+      }
+    }));
+    onSubmit.mockReset();
+  })
+
+  test('if clicking the sign in button signs the user in', async () => {
+    const { getByText } = render(<SignInForm onSubmit={onSubmit} />);
+
+    const button = getByText('Sign In');
+    await act(async () => {
+      await fireEvent.click(button);
+    });
+
+    expect(onSubmit).toHaveBeenCalled();
+  });
+
+  test('if SignIn shows an error when sign in fails', async () => {
+    signUserIn.mockImplementation(() => Promise.reject({
+      statusCode: 400,
+      message: 'This failed dude!'
+    }));
+
+    const { getByText } = render(<SignInForm onSubmit={onSubmit} />);
+
+    const signInButton = getByText('Sign In');
+    await act(async () => {
+      await fireEvent.click(signInButton);
+    });
+
+    expect(getByText('This failed dude!')).not.toBeNull();
+    expect(onSubmit).not.toHaveBeenCalled();
+  });
+
+  test('if SignIn shows a loading animation while waiting for sign in to complete', async () => {
+    const { getByText } = render(<SignInForm onSubmit={onSubmit} />);
+
+    const button = getByText('Sign In');
+    expect.assertions(1);
+
+    await act(async () => {
+      await fireEvent.click(button);
+      expect(getByText('loading...')).not.toBeNull();
+    });
+  });
 });
